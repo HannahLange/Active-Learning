4fcd36cbc5ee3cc211ef45966bc68f57864e65aa
HannahLange <82364625+HannahLange@users.noreply.github.com>
2022-10-07 11:10:29 +0200

    Add files via upload

---
+++ Paper/IBM_states/Tests/x-spins/quantum/5_qubits_new2/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Test
+#SBATCH --comment="IBMW3"
+#SBATCH --mem=40960
+#SBATCH --time=60:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hannah.lange@physik.uni-muenchen.de
+#SBATCH --chdir=
+#SBATCH --output=slurm.%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --constraint=avx2
+
+
+source /project/th-scratch/m/Maximilian.Buser/syten_pub/sytensetup
+source /project/th-scratch/h/Hannah.Lange/Projektpraktikum/ENVDIR/bin/activate
+python3 example.py
